4756dfe44b13e04902f1808cc3bd71e9964597f9
Neo <neo.chan@live.com>
2014-07-23 09:37:45 +0800

    new file:   test/server.php

---
+++ test/server.php
@@ -0,0 +1,16 @@
+<?php
+
+/*   
+ *       The server waits for messages from the client
+ *           and echoes back the received message
+ *           */
+$server = new ZMQSocket(new ZMQContext(), ZMQ::SOCKET_REP);
+//$server->bind("tcp://127.0.0.1:5555");
+$server->bind("tcp://192.168.2.1:5555");
+
+/* Loop receiving and echoing back */
+while ($message = $server->recv()) {
+      echo "Got message: $message\n";
+          /* echo back the message */
+          $server->send($message);  
+}
